75d027b68827f2b641189d0748211aaf1a8119cb
Teilor Souza Barcelos <teilor.barcelos@uds.com.br>
2022-09-27 23:08:36 -0300

    routing config done

--- src/app/core/components/auth/auth-routing.module.ts
+++ src/app/core/components/auth/auth-routing.module.ts
@@ -1,10 +1,16 @@
-import { NgModule } from '@angular/core';
-import { RouterModule, Routes } from '@angular/router';
+import { NgModule } from '@angular/core'
+import { RouterModule, Routes } from '@angular/router'
+import { SignComponent } from './pages/sign/sign.component'
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    component: SignComponent,
+  },
+]
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AuthRoutingModule { }
+export class AuthRoutingModule {}
